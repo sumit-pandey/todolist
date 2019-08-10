905d843ce8ad0d2c465439640b22f23a4eea23cb
Sumit Pandey <sumit_pandey@outlook.com>
2019-08-11 01:28:42 +0530

    initial commit

---
+++ precache-manifest.8229016b45046747f1aabeacda4b5e83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5593334f22882d6b123da28a75469c7",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "9a0b3fa305adfb20e4d1",
+    "url": "/todolist/static/css/main.86ad3fce.chunk.css"
+  },
+  {
+    "revision": "565a655b23b2284d7016",
+    "url": "/todolist/static/js/2.6c306069.chunk.js"
+  },
+  {
+    "revision": "9a0b3fa305adfb20e4d1",
+    "url": "/todolist/static/js/main.9fda2a50.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
